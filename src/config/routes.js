ce807655e91fc987f2f3aeffcfceb436c97f5f66
Rodrigo Santos <rodriigoacs@gmail.com>
2020-03-29 21:10:10 -0300

    feat(screen): create Identification screen ands childrens

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import { IdentificationScreen } from 'screens/Identification';
 import { Profile as ProfileScreen, SuccessScreen } from 'screens/Profile';
 import { PreConditionsScreen } from 'screens/PreConditions';
 import { RiskScreen } from 'screens/Risk';
@@ -18,8 +20,9 @@ const Stack = createStackNavigator();
 
 const Navigator = () => (
   <NavigationContainer>
-    <Stack.Navigator headerMode='none' initialRouteName='Profile'>
+    <Stack.Navigator headerMode='none' initialRouteName='Identification'>
       <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Identification' component={IdentificationScreen} />
       <Stack.Screen name='Profile' component={ProfileScreen} />
       <Stack.Screen name='PreConditions' component={PreConditionsScreen} />
       <Stack.Screen name='Success' component={SuccessScreen} />
---
+++ src/containers/Identification/Identification.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IdentificationPresentation } from 'components/presentation/Identification';
+
+const IdentificationContainer = () => {
+  return <IdentificationPresentation />;
+};
+
+export default IdentificationContainer;
