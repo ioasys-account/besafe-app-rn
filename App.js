094822e6a1cbb6803c27fb9c14491b7a0ada8318
Guilherme Bussinger <guilhermepedro@ioasys.com.br>
2020-04-01 02:42:06 -0300

    fixing get locale

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import 'react-native-gesture-handler';
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import { KeyboardAvoidingView, Platform, StatusBar, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 
 import store from 'store/reducerConfig';
 
 import Navigator from 'config/routes';
 import COLORS from 'config/colors';
 
-import { getLanguage } from 'helpers';
-
 const App = () => {
-  const { i18n } = useTranslation();
-
-  useLayoutEffect(() => {
-    i18n.changeLanguage(getLanguage());
-  }, []);
-
   return (
     <Provider store={store}>
       <StatusBar backgroundColor={COLORS.background} barStyle='dark-content' />
--- src/helpers/getLanguage.js
+++ src/helpers/getLanguage.js
@@ -3,8 +3,9 @@ import { NativeModules, Platform } from 'react-native';
 export default () => {
   const locale =
     Platform.OS === 'ios'
-      ? NativeModules.SettingsManager.settings.AppleLocale
+      ? NativeModules.SettingsManager.settings.AppleLocale ||
+        NativeModules.SettingsManager.settings.AppleLanguages[0]
       : NativeModules.I18nManager.localeIdentifier;
 
-  return locale ? locale.replace('_', '-') : 'en-us';
+  return locale.split('-')[0];
 };
