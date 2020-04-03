39a2209e2e5e6583a8024f7ee0adc3f093a85a83
Guilherme Bussinger <guilhermepedro@ioasys.com.br>
2020-04-03 04:56:16 -0300

    creatin splash screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { KeyboardAvoidingView, Platform, StatusBar, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 import store from 'store/reducerConfig';
 
@@ -9,6 +10,10 @@ import Navigator from 'config/routes';
 import COLORS from 'config/colors';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <StatusBar backgroundColor={COLORS.background} barStyle='dark-content' />
